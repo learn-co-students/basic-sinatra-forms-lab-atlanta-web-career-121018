43956c8e1f26c317909db8ab7280792c3a871697
github username <github email address>
2019-01-07 17:16:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @team_attributes = params
+        # binding.pry # what is params.class? Is a string before being saved to @team_attributes
+        # Once saved to @team_attributes, it becomes a hash.
+        # @name = params[:name]
+        # @coach = params[:coach]
+        # @pg = params[:pg]
+        # @sg = params[:sg]
+        # @pf = params[:pf]
+        # @sf = params[:sf]
+        # @c = params[:c]
+        erb :team
+    end
+
 
 end
